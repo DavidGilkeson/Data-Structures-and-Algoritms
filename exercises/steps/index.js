68abf5ef1402975c692886eaa5cbbae2461aab3f
David Gilkeson <davidgilkeson8@gmail.com>
2020-06-22 01:37:24 +0100

    Solution added for steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -19,11 +19,27 @@
 
 function steps(n) {
 
-  let input = prompt("Please enter a positive number");
+
+
+
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+
+    for (let col = 0; col < n; col++) {
+
+      if (col <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
 
 
 }
-debugger;
-steps(4);
+
 
 module.exports = steps;
+
+//node --inspect-brk
